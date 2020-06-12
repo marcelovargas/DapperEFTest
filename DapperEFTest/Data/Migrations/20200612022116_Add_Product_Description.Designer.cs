e82b33882866266f05c67212c67c97287e1b40cf
Marcelo Vargas <themartrix@hotmail.com>
2020-06-11 23:22:15 -0300

    Migration.

--- DapperEFTest/Data/Migrations/20200612022116_Add_Product_Description.Designer.cs
+++ DapperEFTest/Data/Migrations/20200612022116_Add_Product_Description.Designer.cs
@@ -4,14 +4,16 @@ using DapperEFTest.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DapperEFTest.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200612022116_Add_Product_Description")]
+    partial class Add_Product_Description
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
